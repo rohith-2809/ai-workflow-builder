a0007a7ff3774518514ec927953c8549716f25f1
rohith-2809 <158805322+rohith-2809@users.noreply.github.com>
2025-07-27 19:30:27 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
-import { ReactFlowProvider } from 'reactflow';
-import { WorkflowProvider } from './context/WorkflowContext.jsx';
-import { ToastContainer } from 'react-toastify';
+import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ReactFlowProvider } from "reactflow";
+import App from "./App.jsx";
+import { WorkflowProvider } from "./context/WorkflowContext.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ReactFlowProvider>
-      <WorkflowProvider>
-        <App />
-        <ToastContainer
-          position="bottom-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-        />
-      </WorkflowProvider>
-    </ReactFlowProvider>
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ReactFlowProvider>
+    <WorkflowProvider>
+      <App />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </WorkflowProvider>
+  </ReactFlowProvider>
 );
